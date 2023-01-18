f3ed17f58e272bd6bb7a5f25bb32abdf4106d602
alok2811 <am5803@gmail.com>
2023-01-18 23:25:11 +0530

    Initial commit

---
+++ lib/triangle_painter.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TrianglePainter extends CustomPainter {
+  final Color color;
+  TrianglePainter({required this.color});
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = color
+      ..strokeWidth = 2.0;
+    Path path = Path();
+    path.moveTo(0.0, size.height);
+    path.lineTo(size.width, 0.0);
+    path.lineTo(size.width, size.height);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
